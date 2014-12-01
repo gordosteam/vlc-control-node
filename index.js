20e6a116d2f2c29e9882eb7a058751a47f1b4ce7
Eduardo Malherbi <emalherbi@gmail.com>
2014-12-01 21:45:39 -0200

    add gitignore

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ function getOptions(path) {
 			'Content-Type' : 'application/json'
 		}
 	};
-
 }
 
 function request(options) {
@@ -49,8 +48,9 @@ function request(options) {
 				console.error(options);
 			}
 		});
-	   
+
 	});
+
 	req.on('error', function(err) {
 		console.error(err.stack);
 		console.error(options);
@@ -80,53 +80,51 @@ function request(options) {
 		var options = getOptions("in_enqueue&input=" + uri);
 		return request(options);
 	};
-	
+
 	this.play = function(id) {
-		if (id) 
+		if (id)
 		  var options = getOptions('pl_play&id=' + id);
-		else 
+		else
 		  var options = getOptions('pl_play');
 		return request(options);
 	};
-	
+
 	this.pause = function(id) {
-		if (id) 
+		if (id)
 		  var options = getOptions('pl_pause&id=' + id);
-		else 
+		else
 		  var options = getOptions('pl_pause');
 		return request(options);
 	};
-	
+
 	this.forceResume = function() {
 		var options = getOptions('pl_forceresume');
 		return request(options);
 	};
-	
+
 	this.forcePause = function() {
 		var options = getOptions('pl_forcepause');
 		return request(options);
 	};
-	
+
 	this.stop = function() {
 		var options = getOptions('pl_stop');
 		return request(options);
 	};
-	
+
 	this.next = function() {
 		var options = getOptions('pl_next');
 		return request(options);
 	};
-	
+
 	this.previous = function() {
 		var options = getOptions('pl_previous');
 		return request(options);
 	};
-	
+
 	this.delete = function(id) {
 		var options = getOptions('pl_delete&id=' + id);
 		return request(options);
 	};
-	
-	  
 
 }).call(vlcControl);
